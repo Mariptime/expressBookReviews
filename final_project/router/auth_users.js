7612e2283c9ab6e3850decc932040ace7d5ab038
Akshay Nagarajan <akshay.h.nagarajan@gmail.com>
2023-10-18 18:04:52 -0400

    Final Version

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -3,37 +3,32 @@ const jwt = require('jsonwebtoken');
 let books = require("./booksdb.js");
 const regd_users = express.Router();
 
+let users = [{"username":"dennis","password":"abc"}];
 
-let users = []
-
-const isValid = (username)=>{
-  let userswithsamename = users.filter((user)=>{
-    return user.username === username
-  });
-  if(userswithsamename.length > 0){
-    return true;
-  } else {
-    return false;
-  }
+const isValid = (username)=>{ //returns boolean
+//write code to check is the username is valid
+    const userMatches = users.filter((user) => user.username === username);
+    return userMatches.length > 0;
 }
 
-const authenticatedUser = (username,password)=>{
-  let validusers = users.filter((user)=>{
-    return (user.username === username && user.password === password)
-  });
-  if(validusers.length > 0){
-    return true;
-  } else {
-    return false;
-  }
+const authenticatedUser = (username,password)=>{ //returns boolean
+//write code to check if username and password match the one we have in records.
+  const matchingUsers = users.filter((user) => user.username === username && user.password === password);
+  return matchingUsers.length > 0;
 }
+
+
+
+//only registered users can login
 
 regd_users.post("/login", (req,res) => {
+  //Write your code here
+  console.log("login: ", req.body);
   const username = req.body.username;
   const password = req.body.password;
 
   if (!username || !password) {
-      return res.status(404).json({message: "Error logging in"});
+    return res.status(404).json({message: "Error logging in"});
   }
 
   if (authenticatedUser(username,password)) {
@@ -42,34 +37,44 @@ regd_users.post("/login", (req,res) => {
     }, 'access', { expiresIn: 60 * 60 });
 
     req.session.authorization = {
-      accessToken,username
+            accessToken,username
+        }
+        return res.status(200).send("User successfully logged in");
+    } else {
+        return res.status(208).json({message: "Invalid Login. Check username and password"});
+    }
+});
+
+// Add a book review
+regd_users.put("/auth/review/:isbn", (req, res) => {
+  //Write your code here
+    const isbn = req.params.isbn;
+    const review = req.body.review;
+    const username = req.session.authorization.username;
+    console.log("add review: ", req.params, req.body, req.session);
+    if (books[isbn]) {
+        let book = books[isbn];
+        book.reviews[username] = review;
+        return res.status(200).send("Review successfully posted");
+    }
+    else {
+        return res.status(404).json({message: `ISBN ${isbn} not found`});
+    }
+});
+
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+  const isbn = req.params.isbn;
+  const username = req.session.authorization.username;
+  if (books[isbn]) {
+      let book = books[isbn];
+      delete book.reviews[username];
+      return res.status(200).send("Review successfully deleted");
   }
-  return res.status(200).send("User successfully logged in");
-  } else {
-    return res.status(208).json({message: "Invalid Login. Check username and password"});
+  else {
+      return res.status(404).json({message: `ISBN ${isbn} not found`});
   }
 });
 
-regd_users.put("/auth/review/:isbn", (req, res) => {
-    const isbn = req.params.isbn
-	  const review = req.body.review
-	if (books[isbn]) {
-		let book = books[isbn]
-		book.reviews[review] = review
-		return res.status(200).send('Review posted')
-	} else {
-		return res.status(404).json({message: `ISBN ${isbn} not found`})
-	}
-})
-
-regd_users.delete("/auth/review/:isbn", (req, res) => { 
-    let username = req.session.authorization.username;
-    let isbn = req.params.isbn;
-    if(isbn){
-      delete books[isbn].reviews[username];
-    }
-    res.send(`User ${username} review on book with ISBN ${isbn} successfully deleted.`);
-  })
 
 
 module.exports.authenticated = regd_users;
